c348a7b9ef2bdbfc65acd26d095228d127f6b400
Renan Costa <91814882+renanzitoo@users.noreply.github.com>
2023-02-01 02:30:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+alert("Ola senhorita Boleta, aqui está uma mensagem de Renan para voce");
+alert("Desculpa pela discussão de ontem, eu ando sendo um panaca com voce");
+alert("Mas hoje a senhorita tem uma prova e eu tenho certeza que vc vai conseguir amassar essa prova");
+
+do{
+var frase = prompt("Essa prova vai ser moleza né?(responde com sim ou não)");
+if (frase == "sim"){
+    alert("Eu sabia que vc ia arrasar garota");
+    alert("Agora va la e detona essa prova")
+} else if (frase == "não"){
+    alert("Vai ser sim garota acorda, vc estudou pra carai e vai arrasar");
+}else {
+    alert("Por favor digite uma resposta valida")
+}
+}while(frase != "sim")
+
+    var frase2 = prompt ("Eu te amo muitão obrigado por participar");
+
+    alert("Arrasa na prova gatinha");
+
